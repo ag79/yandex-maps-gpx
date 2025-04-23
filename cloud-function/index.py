cbe332b1f8166b4946fee9c3b7556c4917769145
Andrey G. <andrey.glinski@gmail.com>
2025-04-24 00:15:10 +0300

    local test, file naming

--- cloud-function/index.py
+++ cloud-function/index.py
@@ -1,11 +1,11 @@
-import unicodedata
 import urllib.parse
 from gpx_utils import get_yandex_maps_json, get_yandex_track_features, create_gpx
 
 
 def make_filename(string):
     # URL-encode for safety
-    s = ''.join(c if c.isalnum() else '_' for c in unicodedata.normalize('NFD', string))
+    invalid_chars = '<>:"/\\|?* '
+    s = ''.join(c if c not in invalid_chars else '_' for c in string)
     s = s + '.gpx'
     return urllib.parse.quote(s)
 
@@ -30,15 +30,15 @@ def handler(event, context):
     
     lines, placemarks = get_yandex_track_features(yandex_maps_json)
     
-    encoded_filename = make_filename(yandex_maps_json['config']['userMap']['title'])
-    del yandex_maps_json
-    
     line_param = query_params.get('lines', 'tracks')
-    line_param = {parameter: lines if line_param == parameter else None \
+    lines_param = {parameter: lines if line_param == parameter else None \
         for parameter in ("tracks", "routes", "track_segments")}
     
+    encoded_filename = make_filename(yandex_maps_json['config']['userMap']['title'] + '_' + line_param)
+    del yandex_maps_json
+    
     # Note url params are str!
-    gpx = create_gpx(**line_param,
+    gpx = create_gpx(**lines_param,
                      places = placemarks if int(query_params.get('placemarks', 0)) else None, 
                      add_elevation = int(query_params.get('add_elevation', 0)))
 
